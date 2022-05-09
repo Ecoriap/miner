cca50d9abf772f81c78876ba55b992e1b474d768
MyEcoria <doctorborken@gmail.com>
2022-05-09 13:29:57 +0200

    Update mine.py

--- mine.py
+++ mine.py
@@ -1,27 +1,35 @@
+#!/usr/bin/python -tt
+
 from bitcoin import *
 from urllib.request import urlopen
 import json
 from rich.console import Console
+import time
+import random
 
 console = Console()
-while 0 < 1 :
-	# Generation de l'adresse
-        
-	priv = sha256("truc")
-	pub = privtopub(priv)
-	addr = pubtoaddr(pub)
-	h = history(addr)
+while 0 < 1:
 
-	# Récupération de la solde
-	url = "https://blockchain.info/rawaddr/" + addr
-	response = urlopen(url)
-	data_json = json.loads(response.read())
-	balance = str(data_json["final_balance"])
-	text = "\n Clef privé: " + priv + ", clef publique: " + pub + ", Adresse: " + addr + ", Solde: " + balance
+    # Generation de l'adresse
+    priv = sha256(str(random.randint(0,100)))
+    pub = privtopub(priv)
+    addr = pubtoaddr(pub)
+    h = history(addr)
 
-	with open("adresses.txt", "a") as fichier:
-		fichier.write(text)
+    # Récupération de la solde
+    url = "https://blockchain.info/rawaddr/" + addr
+    response = urlopen(url)
+    data_json = json.loads(response.read())
+    balance = str(data_json["final_balance"])
+    text = "\n Clef privé: " + priv + ", clef publique: " + \
+        pub + ", Adresse: " + addr + ", Solde: " + balance
+
+    if int(balance) > 0:
+        with open("adresses.txt", "a") as fichier:
+            fichier.write(text)
+
+    console.print("[bold cyan]Adresse:[/bold cyan] " + addr + " [bold magenta]=>[/bold magenta] " + balance)
+    time.sleep(10)
 
 
-	console.print ("[bold cyan]Adresse:[/bold cyan] " + addr + " [bold magenta]=>[/bold magenta] " + balance)
         
