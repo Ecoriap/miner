f7c213ca5647810ee98ec6fbd023a3ceb7103f31
MyEcoria <doctorborken@gmail.com>
2022-05-09 12:47:25 +0200

    Update mine.py

--- mine.py
+++ mine.py
@@ -1,10 +1,27 @@
 from bitcoin import *
-priv = sha256("MyEcoria")
-pub = privtopub(priv)
-addr = pubtoaddr(pub)
-h = history(addr)
+from urllib.request import urlopen
+import json
+from rich.console import Console
 
-print (priv)
-print (pub)
-print (addr)
-print (h)
+console = Console()
+while 0 < 1 :
+	# Generation de l'adresse
+        
+	priv = sha256("truc")
+	pub = privtopub(priv)
+	addr = pubtoaddr(pub)
+	h = history(addr)
+
+	# Récupération de la solde
+	url = "https://blockchain.info/rawaddr/" + addr
+	response = urlopen(url)
+	data_json = json.loads(response.read())
+	balance = str(data_json["final_balance"])
+	text = "\n Clef privé: " + priv + ", clef publique: " + pub + ", Adresse: " + addr + ", Solde: " + balance
+
+	with open("adresses.txt", "a") as fichier:
+		fichier.write(text)
+
+
+	console.print ("[bold cyan]Adresse:[/bold cyan] " + addr + " [bold magenta]=>[/bold magenta] " + balance)
+        
